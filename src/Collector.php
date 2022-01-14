ece5229bb5ce28d2c76c04bc1be6f955fe46b506
Jaroslav Bedri <sidha2@gmail.com>
2022-01-14 13:55:40 +0100

    fetch pairs from Binance

--- src/Collector.php
+++ src/Collector.php
@@ -6,15 +6,39 @@ namespace Crypto\PriceCollector;
 
 class Collector implements \JsonSerializable
 {
-    private $data;
+   
     
-    public function __construct(array $data)
+    public function __construct()
     {
-        $this->data = $data;
+
     }
 
     public function jsonSerialize(): array
     {
-        return $this->data;
+        
     }
+
+
+    /**
+     *  Fetch prices from Binance exchange
+     */
+    public function fetchBinancePairs($api, array $pairsToRead_Binance): array
+    {        
+        $ticker = $api->prices();
+        $priceList = [];
+
+        # associate prices to symbols from config
+        foreach ($ticker as $symbol=>$price)
+        {
+            # build array of symbols and prices
+            foreach ($pairsToRead_Binance as $searchedPair)
+            {
+                if ($searchedPair == $symbol) $priceList[$symbol] = $price;
+            }
+        }
+
+        return $priceList;
+    }
+
+
 }
--- tests/CollectorTest.php
+++ tests/CollectorTest.php
@@ -6,14 +6,43 @@ namespace Tests\Crypto\PriceCollector;
 
 use Crypto\PriceCollector\Collector;
 use PHPUnit\Framework\TestCase;
+use Binance\API;
 
 class CollectorTest extends TestCase
-{
-    public function testClassDataCanBeEncoded(): void
+{  
+
+    public function testFetchBinancePairs(): void 
     {
+
+        $pairsToRead_Binance = [
+            "BTCUSDT",
+            "ETHUSDT",
+            "XMRUSDT"
+        ];
+
+        $input = 
+        [
+            'BTCUSDT'    => '41992.73000000',            
+            'ETHUSDT'    => '3216.77000000',
+            'XMRUSDT'    => '218.80000000',
+            'xxxxxx'    => '1111',
+        ];        
+
+        $expect = 
+        [
+            'BTCUSDT' => '41992.73000000',
+            'ETHUSDT' => '3216.77000000',
+            'XMRUSDT' => '218.80000000',
+        ];
+
+        $mock = $this->createMock(API::class);
+        $mock->method('prices')->willReturn($input);
+
         $this->assertEquals(
-            '{"ticker":"BTC"}',
-            json_encode(new Collector(['ticker' => 'BTC']))
+            $expect,
+            (new Collector())->fetchBinancePairs($mock, $pairsToRead_Binance)
         );
+        
     }
+
 }
