a87332273fb60d932dff1388d8501c02eede7eb0
Jaroslav Bedri <sidha2@gmail.com>
2022-01-20 11:54:59 +0100

    remove table sql from config

--- src/config.php
+++ src/config.php
@@ -9,15 +9,6 @@ $dbpass = 'db_password';
 $dbname = 'db_name';
 $dbport = 3306;
 
-/**
- *  Ticker BINANCE Create table Query
- */
-$createBinanceTicker = "CREATE TABLE binance (
-    id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    pair VARCHAR(50) NOT NULL,
-    price VARCHAR(20) NOT NULL,
-    updateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    )";
 
 # list of symbols to read from ticker
 $pairsToRead = [
