97c059279d4e20bb8c66218275bc248e79891cca
Jaroslav Bedri <sidha2@gmail.com>
2022-01-20 12:29:33 +0100

    Run Price Collector

--- src/Collector.php
+++ src/Collector.php
@@ -3,16 +3,24 @@
 declare(strict_types=1);
 
 namespace Crypto\PriceCollector;
+use Crypto\PriceCollector\DB\tickerDB;
+use \Binance\API;
 
 class Collector implements \JsonSerializable
 {
    
-    private $apiBinance;
+    private $apiBinance;        # Binance api
+    private $cycle = false;     # run on/off cycle
+    private $sleep = 5;         # sleeptime of cycle
+    private $pairsToRead;       # pairs from config.php
+    private $tickerDB;          # work with ticker DB
     
-    public function __construct(\Binance\API $apiBinance)
+    public function __construct(API $apiBinance, tickerDB $tickerDB, array $pairsToRead)
     {
-        $this->apiBinance = $apiBinance;            
-        $this->apiBinance->caOverride = true;
+        $this->apiBinance               = $apiBinance;            
+        $this->apiBinance->caOverride   = true;
+        $this->tickerDB                 = $tickerDB;
+        $this->pairsToRead              = $pairsToRead;
     }
 
     public function jsonSerialize(): array
@@ -23,6 +31,7 @@ class Collector implements \JsonSerializable
 
     /**
      *  chose right exchange ticker
+     *  TODO change this to private
      */
     public function fetchPairs($exchange, $pairs): array
     {
@@ -40,12 +49,13 @@ class Collector implements \JsonSerializable
 
     /**
      *  Fetch prices from Binance exchange
+     *  TODO change this to private
      */
-    public function fetchBinancePairs(array $pairsToRead): array
+    public function fetchBinancePairs($pairsToRead): array
     {        
         $ticker = $this->apiBinance->prices();
         $priceList = [];
-
+        
         # associate prices to symbols from config
         foreach ($ticker as $symbol=>$price)
         {
@@ -60,4 +70,144 @@ class Collector implements \JsonSerializable
     }
 
 
+    /**
+     *  Run ticker
+     */
+    public function run()
+    {
+        $cycle = true;
+
+        # run this block atleast once
+        while ($cycle === true )
+        {
+
+            foreach ($this->pairsToRead as $exchange => $tickerPairs)
+            {                    
+
+                $this->tickerDB->setDbName($exchange);
+
+                # connection to DB was not successfull
+                if ($this->tickerDB->connectionToDBError()) {
+                    echo 'Failed to connect to MySQL - ' . $tickerDB->connectionToDBError();
+                    break;
+                }
+
+                # read all prices from exchange
+                $ticker = $this->fetchPairs($exchange, $tickerPairs);
+
+                # if something goes wrong ticker is empty, for example, exchange is in maintenance
+                if (empty($ticker))
+                {
+                    echo "Ticker z burzy '$exchange' sa nepodarilo nacitat.". PHP_EOL;
+                    continue;
+                }
+
+                # main update pairs and prices to DB
+                foreach ($ticker as $pair => $price)
+                {
+                    # try to update price
+                    $updatePrice = $this->tickerDB->update($pair, $price);
+                    if ($updatePrice !== true)
+                    {
+                        # if pair don't exist, create it, otherwise the new price is same as old price
+                        $searchPair = $this->tickerDB->select($pair);
+                        if(!$searchPair)
+                        {
+                            echo "Nepodarilo sa najst par '$pair'". PHP_EOL;
+
+                            # if can't insert pair, need to create table
+                            $insertPair = $this->tickerDB->insert($pair, $price);
+                            if(!$insertPair)
+                            {
+                                echo "Nepodarilo sa vytvorit par '$pair'". PHP_EOL;
+
+                                $createTable = $this->tickerDB->createTable($exchange);
+                                if(!$createTable)
+                                {
+                                    echo $this->tickerDB->getConnectionError();
+                                }
+                                else
+                                {
+                                    echo "Vytvoril som tabulku '$exchange'". PHP_EOL;
+
+                                    $insertPairTo = $this->tickerDB->insert($pair, $price);
+                                    if(!$insertPair)
+                                    {
+                                        echo $this->tickerDB->getConnectionError(). PHP_EOL;
+                                    }
+                                    else
+                                    {
+                                        echo "Vytvoril som novy par '$pair' a pridadil mu cenu: $price". PHP_EOL;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                echo "Vytvoril som novy par '$pair' a pridadil mu cenu: $price". PHP_EOL;
+                            }
+                        }
+                    }
+                }
+                
+            } 
+
+            # turn on/off cycle
+            $cycle = $this->cycle;
+
+            if ($cycle == true)
+            {
+                echo "Pairs updated successful! Starting new cycle... (wait: ".$this->sleep."s)" .PHP_EOL;
+                sleep($this->sleep);
+            }  
+            else
+            {
+                echo "Pairs updated successful!";
+            }        
+
+            
+        }
+
+    }
+
+
+
+    /**
+     * Get the value of cycle
+     */ 
+    public function getCycle()
+    {
+        return $this->cycle;
+    }
+
+    /**
+     * Set the value of cycle
+     *
+     * @return  self
+     */ 
+    public function setCycle($cycle)
+    {
+        $this->cycle = $cycle;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of sleep
+     */ 
+    public function getSleep()
+    {
+        return $this->sleep;
+    }
+
+    /**
+     * Set the value of sleep
+     *
+     * @return  self
+     */ 
+    public function setSleep($sleep)
+    {
+        $this->sleep = $sleep;
+
+        return $this;
+    }
 }
--- tests/CollectorTest.php
+++ tests/CollectorTest.php
@@ -5,22 +5,38 @@ declare(strict_types=1);
 namespace Tests\Crypto\PriceCollector;
 
 use Crypto\PriceCollector\Collector;
+use Crypto\PriceCollector\DB\TickerDB;
 use PHPUnit\Framework\TestCase;
-
+use \Binance\API;
 
 class CollectorTest extends TestCase
 {  
     
+    /**
+     *  Setup
+     */
     public function setUp() : void
     {
-        $this->apiBinance = $this->createMock(\Binance\API::class);
-        $this->Collector = new Collector($this->apiBinance);
-
         $this->pairsToRead = [
             "BTCUSDT",
             "ETHUSDT",
             "XMRUSDT"
         ];
+
+        $this->pairsToReadFull = [
+
+            "binance" => [
+                "BTCUSDT",
+                "ETHUSDT",
+                "XMRUSDT"
+            ],
+        ];
+
+        $this->apiBinance = $this->createMock(API::class);
+        $this->tickerDB = $this->createMock(TickerDB::class);
+        $this->collector = new Collector($this->apiBinance, $this->tickerDB, $this->pairsToReadFull);
+
+        
 
         $this->binanceTicker = 
         [
@@ -34,16 +50,21 @@ class CollectorTest extends TestCase
     }
 
 
+    /**
+     *  Test if method return empty array if bad exchange name was added
+     */
     public function testFetchPairsReturnEmptyAfterBadExchange(): void 
     {
         $pairs = $this->pairsToRead;
         $this->assertEquals(
-            [], ((new Collector($this->apiBinance))->fetchPairs('bitfinex', $pairs))
+            [], ((new Collector($this->apiBinance, $this->tickerDB, $this->pairsToReadFull))->fetchPairs('bitfinex', $pairs))
         );
     }
 
 
-
+    /**
+     *  Test if get pairs after right argument was set
+     */
     public function testFetchRightExchangeData(): void
     {
         $expect = 
@@ -52,15 +73,17 @@ class CollectorTest extends TestCase
             'ETHUSDT' => '3216.77000000',
             'XMRUSDT' => '218.80000000',
         ];   
-        $pairs = $this->pairsToRead;
 
         $this->assertEquals(
             $expect,
-            ((new Collector($this->apiBinance))->fetchPairs('binance', $pairs))
+            ((new Collector($this->apiBinance, $this->tickerDB, $this->pairsToReadFull))->fetchPairs('binance', $this->pairsToRead))
         );
     }
 
 
+    /**
+     *  Test fetch pairs
+     */
     public function testFetchBinancePairs(): void 
     {
         $expect = 
@@ -70,15 +93,118 @@ class CollectorTest extends TestCase
             'XMRUSDT' => '218.80000000',
         ];
 
-        // $mock = $this->createMock(API::class);
-        // $mock->method('prices')->willReturn($this->binanceTicker);
-        $pairs = $this->pairsToRead;
-
         $this->assertEquals(
             $expect,
-            ((new Collector($this->apiBinance))->fetchBinancePairs($pairs))
+            ((new Collector($this->apiBinance, $this->tickerDB, $this->pairsToReadFull))->fetchBinancePairs($this->pairsToRead))
         );
         
     }
 
+
+    /**
+     *  Test if ticker update price
+     */
+    public function testRunUpdatePrice(): void
+    {
+        $this->tickerDB->method('setDbName')->willReturn('binance');
+        $this->tickerDB->method('connectionToDBError')->willReturn(null);
+        // $this->collector->method('fetchPairs')->willReturn($this->binanceTicker);
+        $this->tickerDB->method('update')->willReturn(true);
+
+        (new Collector($this->apiBinance, $this->tickerDB, $this->pairsToReadFull))->run();
+        $this->expectOutputString('Pairs updated successful!');
+    }
+
+
+    /**
+     *  Test if ticker don't find pair
+     */
+    public function testRunDontFindPair(): void
+    {
+        $pairsToReadFull = [
+
+            "binance" => [
+                "BTCUSDT",
+            ],
+        ];
+
+        $this->pairsToRead = [
+            "BTCUSDT",
+        ];
+
+        $this->tickerDB->method('setDbName')->willReturn('binance');
+        $this->tickerDB->method('connectionToDBError')->willReturn(null);
+        $this->tickerDB->method('update')->willReturn(false);
+        
+        (new Collector($this->apiBinance, $this->tickerDB, $pairsToReadFull))->run();
+        $this->expectOutputString(
+            "Nepodarilo sa najst par 'BTCUSDT'\n".
+            "Nepodarilo sa vytvorit par 'BTCUSDT'\n".
+            "Pairs updated successful!"
+         );
+    }
+
+    /**
+     *  Test insert new pair
+     */
+    public function testRunInsertNewPair(): void
+    {
+        $pairsToReadFull = [
+
+            "binance" => [
+                "BTCUSDT",
+            ],
+        ];
+
+        $this->pairsToRead = [
+            "BTCUSDT",
+        ];
+
+        $this->tickerDB->method('setDbName')->willReturn('binance');
+        $this->tickerDB->method('connectionToDBError')->willReturn(null);
+        $this->tickerDB->method('select')->willReturn(false);
+        $this->tickerDB->method('update')->willReturn(false);
+        $this->tickerDB->method('insert')->willReturn(true);
+        //$this->tickerDB->method('createTable')->willReturn(false);
+
+        (new Collector($this->apiBinance, $this->tickerDB, $pairsToReadFull))->run();
+        $this->expectOutputString(
+            "Nepodarilo sa najst par 'BTCUSDT'\n".
+            "Vytvoril som novy par 'BTCUSDT' a pridadil mu cenu: 41992.73000000\n".
+            "Pairs updated successful!"
+         );
+    }
+
+    /**
+     *  Test Create table
+     */
+    public function testRunDontFindTableThenCreateNewTable(): void
+    {
+        $pairsToReadFull = [
+
+            "binance" => [
+                "BTCUSDT",
+            ],
+        ];
+
+        $this->pairsToRead = [
+            "BTCUSDT",
+        ];
+
+        $this->tickerDB->method('setDbName')->willReturn('binance');
+        $this->tickerDB->method('connectionToDBError')->willReturn(null);
+        $this->tickerDB->method('select')->willReturn(false);
+        $this->tickerDB->method('update')->willReturn(false);
+        $this->tickerDB->method('insert')->willReturn(false);
+        $this->tickerDB->method('createTable')->willReturn(true);
+        //$this->tickerDB->method('createTable')->willReturn(false);
+
+        (new Collector($this->apiBinance, $this->tickerDB, $pairsToReadFull))->run();
+        $this->expectOutputString(
+            "Nepodarilo sa najst par 'BTCUSDT'\n".
+            "Nepodarilo sa vytvorit par 'BTCUSDT'\n".
+            "Vytvoril som tabulku 'binance'\n\n".
+            "Pairs updated successful!"
+         );
+    }
 }
