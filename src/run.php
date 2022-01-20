c51fee311bdec1777118ea491cd54bacdc21eb7a
Jaroslav Bedri <sidha2@gmail.com>
2022-01-20 12:54:24 +0100

    setup and run collector

--- src/run.php
+++ src/run.php
@@ -1,40 +1,14 @@
-# Price collector
+<?php
 
-## Usage
+declare(strict_types=1);
 
-Run docker containers
-```
-docker-compose up -d
-```
 
-Web service is available on http://localhost
-
-MariaDB is available from web service container on host `db:3306`, otherwise on `127.0.0.1:3306`
-
-phpMyAdmin is available on http://localhost:8080
-
-___
-*If you want to persist mysql data, to stop containers, use*
-```
-docker-compose stop
-```
-
-## Tests
-
-Run in web-service container
-```
-docker exec -it price-collector_web-service_1 /var/www/html/vendor/bin/phpunit
-```
-
-Run locally using PHP 8.1
-```
-./vendor/bin/phpunit
-```
-
-## Run Price Collector
-Setup Collector
-```
+// require 'vendor/autoload.php';
 require __DIR__ . '/../vendor/autoload.php';
+
+/**
+ *  Include config file
+ */ 
 include("config.php");
 
 use Crypto\PriceCollector\Collector;
@@ -48,11 +22,6 @@ $apiBinance = new API('', '');                                      # API keys n
 $tickerDB   = new TickerDB($db);                                    # work with DB
 $collector  = new Collector($apiBinance, $tickerDB, $pairsToRead);  # pairsToRead set in config.php
 $collector->setCycle(true)->setSleep(5);                            # set up collector to loop with sleep time
-```
 
-Run as CLI
-```
 # RUN price collector
 $collector->run();
-```
-
